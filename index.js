27a1a254c3f33603ffcd6b59ce49d2edb091dc79
timothy-miriti <timothy.miriti@student.moringaschool.com>
2026-04-14 13:29:25 +0300

    add async javascript code

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code here!
+async function getPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
+  displayPosts(posts);
+}
+function displayPosts(posts) {
+  const postList = document.getElementById('post-list');
+  posts.forEach(post => {
+    const li = document.createElement('li');
+    const h1 = document.createElement('h1');
+    const p = document.createElement('p');
+
+    h1.textContent = post.title;
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    postList.appendChild(li);
+  });
+}
+getPosts();
+
+
+
+
